43b22119b19961000380a4ca3ee16f332cefbd5b
Zagoshipda <john713@hanmail.net>
2020-05-26 14:15:15 +0900

    Update : server side javascript session 6 - login 3

---
+++ server side javascript/ex82.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: '12343452524',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// session data is stored in memory by default so that it could be removed.
+app.get('/count', function(req, res){
+  if(req.session.count){
+    req.session.count++;
+  }else {
+    req.session.count = 1;
+  }
+  res.send('count : ' + req.session.count);
+});
+
+// confirm session data.
+app.get('/tmp', function(req, res){
+  res.send('result : ' + req.session.count);
+});
+
+app.get('/welcome', function(req, res){
+  // res.send(req.session);
+  if(req.session.displayName){
+    res.send(`
+      <h1>Hello, ${req.session.displayName}</h1>
+      <p>
+        <a href="/welcome">HOME</a>
+      </p>
+      <p>
+        <a href="/auth/logout">Logout</a>
+      </p>
+    `);
+  }else {
+    res.send(`
+      <h1>Welcome</h1>
+      go to <a href="/auth/login">Login</a>
+    `);
+  }
+});
+
+app.post('/auth/login', function(req, res){
+  // login data.
+  var user = {
+    username:'john',
+    password:'111',
+    displayName:'Jonny'
+  };
+  var uname = req.body.username;
+  var pwd = req.body.password;
+  var output = uname + ' ' + pwd;
+  if(uname === user.username && pwd === user.password){
+    // res.send(output + '<br>Hello\nmaster');
+    req.session.displayName = user.displayName;
+    res.redirect('/welcome'); // go back to main page.
+  }else {
+    res.send(output + '<br>Who\nare you? <a href="/auth/login">go back to Login</a>');
+  }
+});
+
+app.get('/auth/login', function(req, res){
+  var output = `
+  <h1>Login</h1>
+  <form action="/auth/login" method="post">
+    <p>
+      <input type="text" name="username" placeholder="username">
+    </p>
+    <p>
+      <input type="password" name="password" placeholder="password">
+    </p>
+    <p>
+      <input type="submit">
+    </p>
+  </form>
+  `;
+  res.send(output);
+});
+
+app.listen(3003, function(){
+  console.log('Connected 3003 port...!');
+});
